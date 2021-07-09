f7690a9c2b070804c4531d2ad30bf2e508f23794
kglavin <kglavin@gmail.com>
2021-07-09 13:55:19 -0700

    train for 250K iterations then play  working

--- aiexplore/rls500/agent.py
+++ aiexplore/rls500/agent.py
@@ -147,7 +147,9 @@ def train():
     agent = Agent()
     game = BallisticGameAI()
     game_reward = 0
-    while True:
+    episode = 250_000
+    while episode > 0:
+        episode -= 1
         hint = False
         # get old state
         state_old = agent.get_state(game)
@@ -155,7 +157,7 @@ def train():
         # get move
         
         # when enabled, gets a hint from the game, to speed up finding good moves.
-        if random.randint(0,3) == 1: # disabled
+        if random.randint(0,100) == 1: # disabled
             final_move = action_onehot[agent._app_specific_hint(game)]
             hint = True
         else:
@@ -206,7 +208,7 @@ def train():
             total_score += score
             mean_score = total_score / agent.n_games
             plot_mean_scores.append(mean_score)
-            plot(plot_scores, plot_mean_scores)
+            plot(plot_scores, plot_mean_scores,'Training')
 
 
 def play(speed=60):
@@ -235,7 +237,7 @@ def play(speed=60):
             game.update_ui()
 
 
-        print("s: ",state[0],  " action: ", action, " ns: ", state_new[0])
+        #print("s: ",state[0],  " action: ", action, " ns: ", state_new[0])
         if done:
             print("--------------------------------------------------------")
             game.reset()
@@ -250,8 +252,8 @@ def play(speed=60):
             total_score += score
             mean_score = total_score / agent.n_games
             plot_mean_scores.append(mean_score)
-            plot(plot_scores, plot_mean_scores)
+            plot(plot_scores, plot_mean_scores,'Playing')
 
 if __name__ == '__main__':
-    #train()
+    train()
     play()
